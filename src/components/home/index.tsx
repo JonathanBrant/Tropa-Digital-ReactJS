deafe0bdd206a09b03986c649d8c8c5ec275f41a
Jonathan Brant <jbrant1995@gmail.com>
2021-05-13 16:38:44 -0300

    bug correction

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -27,6 +27,31 @@ export function Home() {
                     </li>
                 </ul>
             </div>
+
+            <div className={styles.menuToggle} >
+                <img className={styles.image} src="logomenu.svg" alt="Tropa Digital" />
+                <ul>
+                    <li>
+                        <button> <img src="home.svg" alt="Início" className={styles.image2} /> Início</button>
+                    </li>
+                    <li>
+                        <button> <img src="contact.svg" alt="Contatos" className={styles.image2} /> Contatos</button>
+                    </li>
+                    <li>
+                        <button> <img src="relatorio.svg" alt="Relatórios" className={styles.image2} /> Relatórios</button>
+                    </li>
+                    <li>
+                        <button> <img src="contact.svg" alt="Contatos" className={styles.image2} /> Contatos</button>
+                    </li>
+                    <li>
+                        <button> <img src="contact.svg" alt="Contatos" className={styles.image2} /> Contatos</button>
+                    </li>
+                    <li>
+                        <button> <img src="contact.svg" alt="Contatos" className={styles.image2} /> Contatos</button>
+                    </li>
+                </ul>
+            </div>
+
             <main className={styles.bannerContainer}>
                 <div className={styles.welcomeMessage}>
                     <p>Olá <span>Usuário</span></p>
@@ -116,42 +141,3 @@ export function Home() {
     )
 }
 
-{/*
-
-<div className={styles.content}>
-<h2 className={styles.welcome}>Olá <span>Usuário</span></h2>
-<div className={styles.imageContainer}>
-    <div>
-        <img src="image.svg" alt="" />
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesettin..</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesettin..</p>
-    </div>
-    <div>
-        <img src="image.svg" alt="" />
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesettin..</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesettin..</p>
-    </div>
-    <div>
-        <img src="image.svg" alt="" />
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesettin..</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesettin..</p>
-    </div>
-    <div>
-        <img src="image.svg" alt="" />
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesettin..</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesettin..</p>
-    </div>
-    <div>
-        <img src="image.svg" alt="" />
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesettin..</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesettin..</p>
-    </div>
-    <div>
-        <img src="image.svg" alt="" />
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesettin..</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesettin..</p>
-    </div>
-</div>
-</div>
-
-*/}
